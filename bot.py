376276a8cfe6e54cda8d6a6fa0dca57bb9a5cdbf
Sunil Sharma <Arnil1718@gmail.com>
2025-08-31 21:11:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ from utils import cleanup_files, is_valid_url
 
 # Initialize bot
 app = Client(
-    "video_merger_bot",
+    "new-video_merger_bot",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN
